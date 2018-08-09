8846b8da89a8252e2c82cde1f040198a4a8d630f
Stephanie Zuniga <stephaniezuvi@gmail.com>
2018-08-09 10:20:49 -0600

    onclick card

--- src/components/presentational/card/index.js
+++ src/components/presentational/card/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 class Card extends React.Component {
     render() {
+        const imageClick = () => {
+            console.log('Click');
+        }
         return (
             <div className="hh-card">
                 <img className="hh-card__image"
                     src={this.props.imageUri}
                     alt={this.props.title}
+                    onClick={() => imageClick()}
                 />
                 <h2 className="hh-card__title">{this.props.title}</h2>
             </div>
