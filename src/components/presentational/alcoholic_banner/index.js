50107548945cce282bdce076bbc6b84f41c85b57
Stephanie Zuniga <szuniga@konradgroup.com>
2018-08-13 10:44:55 -0600

    unfinished slider banner  with timer

--- src/components/presentational/alcoholic_banner/index.js
+++ src/components/presentational/alcoholic_banner/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
 class Alcoholic_Banner extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            positions : ['top', 'center', 'bottom'],
+            currentIndex : 0
+        };
+      }
+
+    componentDidMount() {
+        this.timer = setInterval(this.tick, 10000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        if(this.state.currentIndex == 2){
+            //this.setState({currentIndex = 0});
+        }else{
+            //this.setState({currentIndex: currentIndex + 1});
+        }
+    }
+
     render() {
+        const styles = {
+            backgroundPosition: this.state.positions[this.state.currentIndex]
+        }
         return (
-            <div className="hh-alcoholic_banner">
+            <div className="hh-alcoholic_banner" style={styles}>
                 <div className="hh-alcoholic_banner__container">
                 </div>
             </div>
